9aba98806c4ed21288a9bee21ee930bf58e43676
Edwin Marte Torres <kdgpro15@gmail.com>
2023-09-11 15:35:56 -0400

    T-5: Working with States

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css'
-import Banner from "./components/Banner.tsx";
 
 function App() {
+    const [name, setName] = useState( 'Mario' )
+
+    const handcleClick = () => {
+        setName('Luigi')
+    }
+
     return (
         <>
-            <Banner></Banner>
+            <div className={ 'App' }>
+                <h1>My name is { name }</h1>
+                <button onClick={ handcleClick }>Change Name</button>
+            </div>
         </>
     )
 }
