023299339a10c8fc19d7b1f65dab7e1b690bca38
Edwin Marte Torres <kdgpro15@gmail.com>
2023-09-11 12:05:00 -0400

    T-2: Testing with variables and JS methods

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,16 @@
 function Banner() {
+    const title: string = "Welcome to the Website"
+    const subtitle: string = "This is a Website for learning REACT"
+    const address: string = "https://www.netninja.dev"
 
     return (
         <>
             <div className={ 'banner' }>
-                <p>Banner</p>
+                <h2>{ title.toUpperCase() }</h2>
+                <p>{ subtitle }</p>
+                <p>Today's date is: { new Date().toDateString() }</p>
+                <p>Random Number: { Math.random() * 100 }</p>
+                <a href={ address }>Visit the Net Ninja Site</a>
             </div>
         </>
     )
