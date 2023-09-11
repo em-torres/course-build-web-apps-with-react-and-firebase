bc98b75245c5daf4c1fd94df3c9796f8649329fb
Edwin Marte Torres <kdgpro15@gmail.com>
2023-09-11 15:45:20 -0400

    T-6: Working with Lists

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import './App.css'
 
 function App() {
     const [name, setName] = useState( 'Mario' )
+    const [events, setEvents] = useState([
+        { title: "Mario's Birthday Bash", id: 1 },
+        { title: "Bowser's Live Stream", id: 2 },
+        { title: "Race on Moo Moo Farm", id: 3 },
+
+    ])
 
     const handcleClick = () => {
         setName('Luigi')
@@ -13,6 +19,13 @@ function App() {
             <div className={ 'App' }>
                 <h1>My name is { name }</h1>
                 <button onClick={ handcleClick }>Change Name</button>
+                {
+                    events.map( (event, index) => (
+                        <div key={ event.id }>
+                            <h2>{ index }.+  { event.title }</h2>
+                        </div>
+                    ))
+                }
             </div>
         </>
     )
