5164e5ff8c1294c2ac15a928a9d73070664028cc
Edwin Marte Torres <kdgpro15@gmail.com>
2023-09-11 11:56:57 -0400

    T01: Creating the first Banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,12 @@
+function Banner() {
+
+    return (
+        <>
+            <div className={ 'banner' }>
+                <p>Banner</p>
+            </div>
+        </>
+    )
+}
+
+export default Banner
