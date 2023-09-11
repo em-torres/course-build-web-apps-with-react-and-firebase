c0046b1dbb58dbf148fa1c49b5c8c84d11e54a21
Edwin Marte Torres <kdgpro15@gmail.com>
2023-09-11 12:12:29 -0400

    T-3: Working with events and functions

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,16 +1,19 @@
 function Banner() {
     const title: string = "Welcome to the Website"
     const subtitle: string = "This is a Website for learning REACT"
-    const address: string = "https://www.netninja.dev"
+
+    // const handleClick = (e: object) => {
+    //     console.log("You clicked me")
+    //     console.log("The site title is: ", title)
+    //     console.log(e)
+    // }
 
     return (
         <>
             <div className={ 'banner' }>
                 <h2>{ title.toUpperCase() }</h2>
                 <p>{ subtitle }</p>
-                <p>Today's date is: { new Date().toDateString() }</p>
-                <p>Random Number: { Math.random() * 100 }</p>
-                <a href={ address }>Visit the Net Ninja Site</a>
+                <button onClick={ () => console.log("hello") }> Click Me</button>
             </div>
         </>
     )
