1f790d1530e04de92a101ab65df10d5d41e987ed
Edwin Marte Torres <kdgpro15@gmail.com>
2023-09-11 15:56:10 -0400

    T-7: Using the Previous State

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,28 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-    const [name, setName] = useState( 'Mario' )
     const [events, setEvents] = useState([
         { title: "Mario's Birthday Bash", id: 1 },
         { title: "Bowser's Live Stream", id: 2 },
-        { title: "Race on Moo Moo Farm", id: 3 },
-
+        { title: "Race on Moo Moo Farm", id: 3 }
     ])
 
-    const handcleClick = () => {
-        setName('Luigi')
+    const handcleClick = ( id: number ) => {
+        setEvents( (prevEvents) => {
+            return prevEvents.filter( (event) => {
+                return id !== event.id
+            })
+        })
     }
 
     return (
         <>
             <div className={ 'App' }>
-                <h1>My name is { name }</h1>
-                <button onClick={ handcleClick }>Change Name</button>
                 {
                     events.map( (event, index) => (
                         <div key={ event.id }>
-                            <h2>{ index }.+  { event.title }</h2>
+                            <h2>{ index }. { event.title }</h2>
+                            <button onClick={ () => handcleClick(event.id) }>Delete Event</button>
                         </div>
                     ))
                 }
