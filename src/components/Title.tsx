c736a9f33534b9c2d9faeb425583802595702acc
Edwin Marte Torres <kdgpro15@gmail.com>
2023-09-12 15:03:12 -0400

    T-16: Adding conditional CSS Styling

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,9 +3,11 @@ import './styles/Title.css'
 export default function Title({ title, subtitle } : any ) {
     return (
         <>
-            <h1 className={ "title" }>{ title }</h1>
-            <br/>
-            <h2 className={ "subtitle" }>{ subtitle }</h2>
+            <div className={ "title-block" }>
+                <h1 className={ "title" }>{ title }</h1>
+                <br/>
+                <h2 className={ "subtitle" }>{ subtitle }</h2>
+            </div>
         </>
     )
 }
