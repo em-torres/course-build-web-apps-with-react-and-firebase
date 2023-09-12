84a27e9cbd7015255b605630fec3d8d00a1df8f1
Edwin Marte Torres <kdgpro15@gmail.com>
2023-09-12 15:07:34 -0400

    T-17: Inline Styling in React

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,7 +5,11 @@ export default function Modal( { children, handleClose }: any) {
     return ReactDOM.createPortal( (
         <>
             <div className={ "modal-backdrop" }>
-                <div className={ "modal" }>
+                <div className={ "modal" } style={ {
+                    border: "4px solid",
+                    borderColor: "#ff4500",
+                    textAlign: "center"
+                } }>
                     { children }
                     <button onClick={ handleClose }>Close</button>
                 </div>
