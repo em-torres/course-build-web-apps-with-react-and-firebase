38700bcabbfff1723edaa0a781adcfe77055f96f
Edwin Marte Torres <kdgpro15@gmail.com>
2023-09-13 14:31:13 -0400

    T-22: Adding OnChange events

--- src/components/NewEventForm.tsx
+++ src/components/NewEventForm.tsx
@@ -1,19 +1,22 @@
 import styles from './styles/NewEventForm.module.css'
+import {useState} from "react";
 
 export default function NewEventForm () {
+    const [title, setTitle] = useState<string>('')
+    const [date, setDate] = useState<string>('')
+
     return (
-        <>
-            <form className={ styles.newEventForm }>
-                <label>
-                    <span>Event Title:</span>
-                    <input type="text"/>
-                </label>
-                <label>
-                    <span>Event Date:</span>
-                    <input type="date"/>
-                </label>
-                <button>Submit</button>
-            </form>
-        </>
+        <form className={ styles.newEventForm }>
+            <label>
+                <span>Event Title:</span>
+                <input type="text" onChange={ (e) => { setTitle(e.target.value) } } />
+            </label>
+            <label>
+                <span>Event Date:</span>
+                <input type="date" onChange={ (e) => { setDate(e.target.value) } } />
+            </label>
+            <button>Submit</button>
+            <p>Title - { title } | Date: { date }</p>
+        </form>
     );
 }
