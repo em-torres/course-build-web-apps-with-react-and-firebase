b994653f5d890ac94e4f522937dd87299a844778
Edwin Marte Torres <kdgpro15@gmail.com>
2023-09-13 14:45:04 -0400

    T-23: Controlled Inputs

--- src/components/NewEventForm.tsx
+++ src/components/NewEventForm.tsx
@@ -5,18 +5,30 @@ export default function NewEventForm () {
     const [title, setTitle] = useState<string>('')
     const [date, setDate] = useState<string>('')
 
+    const resetForm = () => {
+        setTitle('')
+        setDate('')
+    }
+
     return (
         <form className={ styles.newEventForm }>
             <label>
                 <span>Event Title:</span>
-                <input type="text" onChange={ (e) => { setTitle(e.target.value) } } />
+                <input type="text"
+                       onChange={ (e) => { setTitle(e.target.value) } }
+                       value={ title }
+                />
             </label>
             <label>
                 <span>Event Date:</span>
-                <input type="date" onChange={ (e) => { setDate(e.target.value) } } />
+                <input type="date"
+                       onChange={ (e) => { setDate(e.target.value) } }
+                       value={ date }
+                />
             </label>
             <button>Submit</button>
             <p>Title - { title } | Date: { date }</p>
+            <p onClick={ resetForm }>Reset the form</p>
         </form>
     );
 }
